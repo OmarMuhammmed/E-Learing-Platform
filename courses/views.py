39adc34e46f6ae722fa88addd5a61940b6aaaa9f
omarmuhammmed <amorey2006@gmail.com>
2025-02-17 15:06:34 +0200

    feat:end cms

--- courses/views.py
+++ courses/views.py
@@ -8,6 +8,7 @@ from django.views.generic.base import View, TemplateResponseMixin
 from .forms import ModuleFormSet
 from django.forms.models import modelform_factory
 from django.apps import apps
+from braces.views import CsrfExemptMixin, JSONResponseMixin
 
 # ----- Mixin-Based Architecture -----
 class ManageCourseListView(ListView):
@@ -144,3 +145,27 @@ class ContentDeleteView(View):
         content.item.delete()
         content.delete()
         return redirect('module_content_list', module_id=module.id)  
+    
+class ModuleContentListView(TemplateResponseMixin, View):
+    template_name = 'courses/manage/module/content_list.html'
+
+    def get(self, request, module_id):
+        module = get_object_or_404(Module,
+                                 id=module_id,
+                                 course__owner=request.user)
+        return self.render_to_response({'module':module})  
+
+class ModuleOrderView(CsrfExemptMixin, JSONResponseMixin, View):      
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Module.objects.filter(id=id, course__owner=request.user).update(order=order)
+
+        return self.render_json_response({'saved': 'OK'})    
+    
+
+class ContentOrderView(CsrfExemptMixin, JSONResponseMixin, View):    
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Content.objects.filter(id=id, module__course__owner=request.user).update(order=order)
+
+        return self.render_json_response({'saved': 'OK'})
